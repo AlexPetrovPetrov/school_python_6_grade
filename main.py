fda7b28a6921576c7401ea0f1ac7955041d0414b
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-03-09 15:54:25 +0200

    using if condition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-a = 12
-b = 6
-c = False
-d = True
-print(a >= b)
-print(a == b)
-print(c < d)
-print(d * a < b)
+my_hidden_word = "chest"
+hidden_word = input("Enter the hidden word ")
+if hidden_word == my_hidden_word:
+    print('Wow you guessed it')
+else:
+    print("You will never be able to guess it ")
