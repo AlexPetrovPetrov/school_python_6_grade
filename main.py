532c77a01b75e23dc0928cee31eea77e1a1a50a1
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-03-17 17:50:35 +0200

    using for loops

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-my_hidden_word = "chest"
-hidden_word = input("Enter the hidden word ")
-if hidden_word == my_hidden_word:
-    print('Wow you guessed it')
-else:
-    print("You will never be able to guess it ")
+# for loop
+# for i in range(10):
+#     print("Hello world",i)
+# animals = ["cat","dog","caw","monkey","snake"]
+# for animal in animals:
+#     print("I like a ",animal)
+# word = "Hello"
+
+# reverse_word = ""
+
+# for letter in word:
+#     print(letter)
+#     if letter == "l":
+#         reverse_word = "x" + reverse_word
+#     else: 
+#         reverse_word = letter + letter + reverse_word
+# print(reverse_word)
