172e7d042dfe95a2d3e84ba2f3d1e1461e894ecb
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-03-22 09:37:15 +0200

    using while for an intersting program

--- main.py
+++ main.py
@@ -23,18 +23,22 @@
 # print("The word "+ word + " contains "+ str(counter) + " times letter e.")
     
 
-from turtle import *
-alex = Turtle()
-screen = Screen()
-alex.color("red")
-goto(0,0)
-shape = 50
-shape_length = 30
-alex.fillcolor("red")
-alex.begin_fill()
-for pen in range(shape):
-    alex.forward(shape_length)
-    alex.left(360/shape)
-alex.end_fill()
+# from turtle import *
+# alex = Turtle()
+# screen = Screen()
+# alex.color("red")
+# goto(0,0)
+# shape = 50
+# shape_length = 30
+# alex.fillcolor("red")
+# alex.begin_fill()
+# for pen in range(shape):
+#     alex.forward(shape_length)
+#     alex.left(360/shape)
+# alex.end_fill()
 
-screen.exitonclick()
+# screen.exitonclick()
+sum = 0
+while sum < 10 :
+    print( str(sum) + " is smaller than 10")
+    sum += 2
